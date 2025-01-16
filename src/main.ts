1ea84ffbdb4e8360415d860cf597d4ef2a79d22b
kimmjen <wpals814@gmail.com>
2025-01-16 17:34:19 +0900

    로그, 파이콘 추가

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 정적 파일 제공 (favicon.ico 포함)
+  app.useStaticAssets(path.join(__dirname, '..', 'public'));
 
   // Swagger 설정
   const config = new DocumentBuilder()
@@ -13,11 +19,21 @@ async function bootstrap() {
     .setVersion('1.0') // 버전
     .addBearerAuth() // Bearer 토큰 인증 추가
     .build();
-  // 글로벌 필터 등록
-  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Swagger 문서 생성 및 설정
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); // Swagger UI를 `/api-docs` 경로에 등록
 
-  await app.listen(process.env.PORT ?? 3000);
+  // 글로벌 필터 등록
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const port = process.env.PORT || 3000; // 포트 설정
+  await app.listen(port);
+
+  // 로그 출력
+  const baseUrl = `http://localhost:${port}`;
+  console.log(`\n🚀 Server is running at: ${baseUrl}`);
+  console.log(`📚 API Documentation available at: ${baseUrl}/api-docs\n`);
 }
+
 bootstrap();
